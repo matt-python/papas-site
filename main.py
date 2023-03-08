b1d4c0e308865eaa031f72d0c72f87a6088db65f
matt-python <matt.python86@gmail.com>
2023-03-07 19:40:31 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.widgets import TextArea
+from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap
+import smtplib
+import datetime
+import os
+
+EM_PWD = os.environ.get('PFR_EMAIL_PWD')
+EMAIL = 'matt.python86@gmail.com'
+DEST_EMAIL = 'PFR2014@Outlook.com'
+
+
+class ContactForm(FlaskForm):
+    name = StringField(label='', validators=[DataRequired()], render_kw={'placeholder': 'Name'})
+    email = StringField(label='', validators=[DataRequired(), Email()], render_kw={'placeholder': 'Email'})
+    phone = StringField(label='', validators=[DataRequired(), Length(min=10, max=10)], render_kw={'placeholder': '10-digit phone number. No symbols or spaces.'})
+    message = StringField(label='', validators=[DataRequired()], widget=TextArea(), render_kw={'placeholder': "What's on your mind?"})
+    submit = SubmitField(label='Submit')
+
+
+app = Flask(__name__)
+app.secret_key = "key"
+Bootstrap(app)
+
+
+def send_msg(name, email, phone, message):
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(EMAIL, EM_PWD)
+        connection.sendmail(
+            from_addr=EMAIL,
+            to_addrs=DEST_EMAIL,
+            msg=f"Subject:PFR Message from {name.title()}\n\n"
+                f"Name: {name.title()}\n"
+                f"Email: {email}\n"
+                f"Phone: {phone}\n"
+                f"Message: {message}"
+        )
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    year = datetime.date.today().year
+    contact_form = ContactForm()
+    if contact_form.validate_on_submit():
+        data = request.form
+        send_msg(data['name'], data['email'], data['phone'], data['message'])
+        return render_template('contact.html'), {f'Refresh': f'3; url= {url_for("index")}'}
+    else:
+        return render_template('index.html', form=contact_form)
+    return render_template('index.html', form=contact_form)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
