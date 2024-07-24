68e9c56e126217dab89706ea442b636e46d97ffb
matt-python <120613509+matt-python@users.noreply.github.com>
2024-07-23 21:22:18 -0500

    Update Flask Form on Main

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ DEST_EMAIL = os.environ.get('PAPA_EMAIL')
 class ContactForm(FlaskForm):
     name = StringField(label='', validators=[DataRequired()], render_kw={'placeholder': 'Name'})
     email = StringField(label='', validators=[DataRequired(), Email()], render_kw={'placeholder': 'Email'})
-    phone = StringField(label='', validators=[DataRequired(), Length(min=10, max=10)], render_kw={'placeholder': '10-digit phone number. No symbols or spaces.'})
-    message = StringField(label='', validators=[DataRequired()], widget=TextArea(), render_kw={'placeholder': "What's on your mind?"})
+    phone = StringField(label='', validators=[DataRequired(), render_kw={'placeholder': 'Phone Number'})
+    message = StringField(label='', validators=[DataRequired()], widget=TextArea(), render_kw={'placeholder': "Please type a detailed message."})
     submit = SubmitField(label='Submit')
 
 
